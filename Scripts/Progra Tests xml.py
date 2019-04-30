719f6e5dcb58e2d3b37e34d1a2638fc14a197e36
Carlos Varela <carlosjvare@gmail.com>
2019-04-29 23:31:48 -0600

    Avances xml

--- Scripts/Progra Tests xml.py
+++ Scripts/Progra Tests xml.py
@@ -1,5 +1,8 @@
 #Importacón de librerías
+import xml.etree.ElementTree as ET
 import requests
+from xml.dom import minidom
+
 
 #Variables Globales
 matrizFrases = []
@@ -57,6 +60,29 @@ def determinarCita ():
             cita=cita[0].split (" - ")
     cita.append(diccionario["id"])
     return cita
+def crearXML():
+    global matrizFrases
+    global DiccionarioPersonajes
+    root=ET.Element("Backup")
+    matriz=ET.SubElement(root,"Matriz")
+    for lista in matrizFrases:
+        personaje=ET.SubElement(matriz,lista[0],infoP=lista)
+    #Diccionario=ET.SubElement(root,"Diccionario")
+    tree=ET.ElementTree(root)
+    ET.dump(root)
+   #print(prettify(tree))
+    tree.write("Backup.xml")
+
+    return
+def prettify(elem):
+        rough_string = ET.tostring(elem, 'utf-8')
+        reparsed = minidom.parseString(rough_string)
+        return reparsed.toprettyxml(indent="  ")
+
+
+
+
+
 
 #PP
 while True:
@@ -67,5 +93,6 @@ while True:
         print (matrizFrases)
         print (DiccionarioPersonajes)
         break
+crearXML()
 
 
